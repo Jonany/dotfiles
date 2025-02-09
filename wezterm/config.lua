e2d2de1406816fdc9ba32d31b42f7bc40f2d5fbf
jonany <jonany@github.com>
2025-02-09 16:41:01 -0500

    wezterm config

---
+++ wezterm/config.lua
@@ -0,0 +1,16 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+--if wezterm.gui.get_appearance():find "Dark" then
+config.color_scheme = "tokyonight_moon"
+--else
+--config.color_scheme = "Catppuccin Latte"
+--end
+
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 18.0
+-- and finally, return the configuration to wezterm
+return config
