cf2c2d114fa30f4013193e1bd89e8d646dcb839e
jonany <jonany@github.com>
2025-12-11 12:25:17 -0500

    wezterm key maps

--- config/wezterm/config.lua
+++ config/wezterm/config.lua
@@ -10,7 +10,31 @@ else
   config.color_scheme = "tokyonight_day"
 end
 
+local act = wezterm.action
+config.keys = {
+  { key = 'UpArrow',   mods = 'SHIFT', action = act.ScrollByLine(-1) },
+  { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollByLine(1) },
+  { key = 'PageUp',    mods = 'SHIFT', action = act.ScrollByPage(-0.75) },
+  { key = 'PageDown',  mods = 'SHIFT', action = act.ScrollByPage(0.75) },
+  {
+    key = 'E',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, _, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+}
+
 config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 18.0
+--config.font_size = 18.0
+config.font_size = 24.0
 -- and finally, return the configuration to wezterm
 return config
